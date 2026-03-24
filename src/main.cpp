a159be8104885b531f2e94d65cc9244b0a982a3b
Samu902 <samuelemarrani@gmail.com>
2026-03-24 14:46:43 +0100

    implementazione lettura cmd args

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <sstream>
 #include <string>
 #include <chrono>
 #include <vector>
@@ -7,7 +8,23 @@
 
 enum Approach { SEQUENTIAL, OPENMP, CUDA };
 
-std::string operation_name(Operation operation){
+// funzione helper per leggere i command line args
+template <typename T>
+bool get_cmd_arg(int argc, char* argv[], const std::string arg_name, T& out_value) {
+    std::string full_arg;
+    std::string prefix;
+    for (int i = 1; i < argc; i++) {
+        full_arg = argv[i];
+        prefix = "--" + arg_name + "=";
+        if (full_arg.starts_with(prefix)) {
+            std::istringstream iss(full_arg.substr(prefix.size()));
+            return (bool)(iss >> out_value);
+        }
+    }
+    return false;
+}
+
+std::string operation_name(Operation operation) {
     switch(operation) {
     case EROSION: return "erosion";
     case DILATION: return "dilation";
@@ -21,14 +38,15 @@ std::string operation_name(Operation operation){
 }
 
 // funzione helper per chiedere approccio
-Approach choose_approach() {
+Approach choose_approach(int argc, char* argv[]) {
     int choice;
     std::cout << "Select approach:\n";
     std::cout << "1 - Sequential CPU\n";
     std::cout << "2 - Parallel CPU (OpenMP)\n";
     std::cout << "3 - GPU (CUDA)\n";
-    std::cin >> choice;
-
+    if (!get_cmd_arg(argc, argv, "approach", choice)) {
+        std::cin >> choice;
+    }
     switch(choice) {
         case 1: return SEQUENTIAL;
         case 2: return OPENMP;
@@ -40,11 +58,13 @@ Approach choose_approach() {
 }
 
 // funzione helper per tipo memoria CUDA
-CudaMemoryType choose_memory_type() {
+CudaMemoryType choose_memory_type(int argc, char* argv[]) {
     int choice;
     std::cout << "Select CUDA memory type:\n";
     std::cout << "1 - Global\n2 - Constant\n3 - Shared\n";
-    std::cin >> choice;
+    if (!get_cmd_arg(argc, argv, "memory-type", choice)) {
+        std::cin >> choice;
+    }
     switch(choice) {
         case 1: return GLOBAL;
         case 2: return CONSTANT;
@@ -56,11 +76,13 @@ CudaMemoryType choose_memory_type() {
 }
 
 // funzione helper per operazione morfologica
-Operation choose_operation() {
+Operation choose_operation(int argc, char* argv[]) {
     int choice;
     std::cout << "Select morphological operation:\n";
     std::cout << "1 - Erosion\n2 - Dilation\n3 - Opening\n4 - Closing\n5 - Gradient\n";
-    std::cin >> choice;
+    if (!get_cmd_arg(argc, argv, "operation", choice)) {
+        std::cin >> choice;
+    }
     switch(choice) {
         case 1: return EROSION;
         case 2: return DILATION;
@@ -73,19 +95,28 @@ Operation choose_operation() {
     }
 }
 
-int main() {
+int main(int argc, char* argv[]) {
     std::string in_image_filename;
     int se_size;
 
     // Input interattivo
-    std::cout << "Enter input image file name (.pgm file in images/in folder): ";
-    std::cin >> in_image_filename;
-    std::cout << "Enter structuring element size (odd integer, e.g., 3/5/7): ";
-    std::cin >> se_size;
 
-    Approach approach = choose_approach();
-    CudaMemoryType mem_type = (approach == CUDA) ? choose_memory_type() : GLOBAL;
-    Operation operation = choose_operation();
+    // manca quello della dimensione
+
+    std::cout << "Enter input image file name (.pgm file in images/in folder): ";
+    if (!get_cmd_arg(argc, argv, "in-img", in_image_filename)) {
+        std::cin >> in_image_filename;
+    }
+    std::cout << "Enter structuring element size (odd integer, e.g., 3/5/7): ";
+    if (!get_cmd_arg(argc, argv, "kernel-size", se_size)) {
+        std::cin >> se_size;
+    }
+
+    // mancano anche quelli di repetitions e cold start -> fare funzione unica dei params?
+
+    Approach approach = choose_approach(argc, argv);
+    CudaMemoryType mem_type = (approach == CUDA) ? choose_memory_type(argc, argv) : GLOBAL;
+    Operation operation = choose_operation(argc, argv);
 
     // Caricamento immagine
     Image in_img = load_image("../images/in/" + in_image_filename + ".pgm");
