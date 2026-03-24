929aa7044e9251dd6025d7ee2abb917ada8d31f3
Samu902 <samuelemarrani@gmail.com>
2026-03-24 17:31:42 +0100

    script test

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -5,11 +5,11 @@ EXECUTABLE="./morphological-image-processing"
 IN_IMG_DIR="./images/in"
 
 # Opzioni possibili dei parametri
+IMAGE_SIZES=(1 2 3)           # 256x256, 512x512, 1024x1024
+SE_SIZES=(1 2 3)              # 3x3, 5x5, 7x7
+OPERATIONS=(1 2 3 4 5)        # erosion, dilation, opening, closing, gradient
 APPROACHES=(1 2 3)            # seq, omp, cuda
 MEM_TYPES=(1 2 3)             # global, constant, shared memory
-OPERATIONS=(1 2 3 4 5)        # erosion, dilation, opening, closing, gradient
-KERNEL_SIZES=(3 5 7)          # 3x3, 5x5, 7x7
-IMAGE_SIZES=(256 512 1024)    # 256x256, 512x512, 1024x1024
 REPETITIONS=10                # default: 10
 COLD_START_REPETITIONS=2      # default: 2
 
@@ -29,21 +29,19 @@ fi
 for file in "$IN_IMG_DIR"/*.pgm; do
   [ -f "$file" ] || continue
 
-  for app in "${APPROACHES[@]}"; do
-    for mem in "${MEM_TYPES[@]}"; do
-      for op in "${OPERATIONS[@]}"; do
-        for k in "${KERNEL_SIZES[@]}"; do
-          for img in "${IMAGE_SIZES[@]}"; do
-
-            #echo "Eseguo: $file | $app | $mem | $op | k=$k | img=$img"
+for img_size in "${IMAGE_SIZES[@]}"; do
+  for se_size in "${SE_SIZES[@]}"; do
+    for op in "${OPERATIONS[@]}"; do
+      for app in "${APPROACHES[@]}"; do
+        for mem in "${MEM_TYPES[@]}"; do
 
             "$EXECUTABLE" \
-              --in-img="$file" \
+              --img-size="$img_size" \
+              --in-img-path="$file" \
+              --se-size="$se_size" \
+              --operation="$op" \
               --approach="$app" \
               --memory-type="$mem" \
-              --operation="$op" \
-              --kernel-size="$k" \
-              --img-size="$img" \
               --repetitions="$REPETITIONS" \
               --cold-start-repetitions="$COLD_START_REPETITIONS"
 
